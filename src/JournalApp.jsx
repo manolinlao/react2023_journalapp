76ec9c6985140b772ec3e7cffc503c694b11af91
mlao <manolinlao@gmail.com>
2023-01-30 21:02:20 +0100

    inicio

---
+++ src/JournalApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
+
+const JournalApp = () => {
+  return (
+    <div>
+      JournalApp
+      <hr />
+      <AppRouter />
+    </div>
+  );
+};
+
+export default JournalApp;
