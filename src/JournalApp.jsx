d8b7c1c8aae73c769a99a01433a6836bc56e71f8
mlao <manolinlao@gmail.com>
2023-01-31 20:37:07 +0100

    mui

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme';
 
 const JournalApp = () => {
   return (
-    <div>
+    <AppTheme>
       JournalApp
       <hr />
       <AppRouter />
-    </div>
+    </AppTheme>
   );
 };
 
